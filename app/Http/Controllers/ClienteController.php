0f341667c541ede78197ea98701594e27aaab951
AlbertoRoman1992 <albertoroman92@hotmail.com>
2017-03-20 18:33:29 +0100

    Descomentada una ruta necesaria

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -13,7 +13,10 @@ class ClienteController extends Controller
      */
     public function index()
     {
-        return view('proyectos.index');
+        $user = \Auth::user();
+        $proyectos = $user->proyectos;
+
+        return view('cliente.index', compact('proyectos', 'user'));
     }
 
     /**
@@ -23,7 +26,7 @@ class ClienteController extends Controller
      */
     public function create()
     {
-        //
+        return view('cliente.create');
     }
 
     /**
@@ -34,7 +37,22 @@ class ClienteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'nombre' => 'required|min:5',
+            'configuracion' => 'required'
+        ]);
+
+        $proyecto = new \App\Proyecto();
+
+        $proyecto->nombre = $request->input('nombre');
+        $proyecto->configuracion = $request->input('configuracion');
+        $proyecto->fecha_creacion= "01/01/01"; // Esta línea se elimina cuando esté funcionando (o se mete la fecha real si no se hace automáticamente)
+        $proyecto->id_cliente = \Auth::user()->id;
+        $proyecto->id_plano = 0; // Hay que meter el plano con $proyecto->id_plano = $request->input('id_plano');
+        $proyecto->save();
+
+        $request->session()->flash('alert-success', 'Proyecto creado con éxito.');
+        return redirect()->route('cliente.index');
     }
 
     /**
@@ -45,7 +63,13 @@ class ClienteController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = \Auth::user();
+        $proyecto = \App\Proyecto::findOrFail($id);
+
+        if ($proyecto->cliente_id != $user->id)
+            return redirect()->route('cliente.index');
+
+        return view('cliente.show', compact('proyecto'));
     }
 
     /**
@@ -79,6 +103,27 @@ class ClienteController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // Mandar un mensaje de confirmacion
+
+        \App\Proyecto::destroy($id);
+
+        // Mensaje de feeback. Un alert de bootstrap
+
+        $request->session()->flash('alert-danger', 'Proyecto eliminado con éxito.');
+        return redirect()->route('cliente.index');
+    }
+
+    public function redireccion()
+    {
+        return redirect()->route('cliente.index');
+    }
+
+    public function cambiar_estado($id)
+    {
+        $proyecto = \App\Proyecto::findOrFail($id);
+        $proyecto->estado == 1;
+        $proyecto->save();
+
+        return $proyecto;
     }
 }
