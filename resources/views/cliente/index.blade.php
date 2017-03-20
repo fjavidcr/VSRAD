0beccc02f996de80247e38850dcea2a7f5d1e313
AlbertoRoman1992 <albertoroman92@hotmail.com>
2017-03-20 18:32:09 +0100

    Interfaz y lógica del cliente (no probado)
    No se puede probar porque faltan cambios de otra rama

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -1,14 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h3> Clientes de {{$user->name}}</h3>
+
+    <div class="container container-page">
+        <div class="row">
+            <div class="col-lg-12">
+                <h3>Proyectos de {{ $user->name }}</h3>
+
+                <a href="{{ route('cliente.create') }}" class="btn btn-primary">
+                    Nuevo proyecto
+                </a>
+            </div>
+        </div>
+
         <div class="row">
             <div class="col-lg-12">
 
-                @if(count($clientes) == 0)
+                @if(count($proyectos) == 0)
                     <div class="alert alert-warning">
-                        <b> No tienes clientes asignados.</b>
+                        <b>Ups!</b> Parece que no tienes proyectos creados
                     </div>
                 @else
 
@@ -17,27 +27,25 @@
                         <thead>
                         <tr>
                             <th>ID</th>
-                            <th>Cliente</th>
+                            <th>Proyecto</th>
                             <th>Estado</th>
                             <th></th>
                         </tr>
                         </thead>
-                        @foreach($clientes as $c)
+                        @foreach($proyectos as $p)
                             <tr>
-                                <td>{{ $c->id }}</td>
-                                <td>{{ $c->name }}</td>
+                                <td>{{ $p->id }}</td>
+                                <td>{{ $p->nombre }}</td>
                                 <td>
-                                    @if($c->validado)
-                                        <p class="labelValidado-{{$c->id}}">Validado</p>
+                                    @if($p->getEstado() != "no_pendiente")
+                                        <p class="labelValidado-{{$p->id}}">{{ $p->getTituloEstado() }}</p>
                                     @else
-                                        <p class="labelValidado-{{$c->id}}">Pendiente de validación</p>
+                                        <a data-id="{{$p->id}}" class="cambiar_estado btn btn-sm btn-primary">Pedir validación</a>
                                     @endif
-                                    <a data-id="{{$c->id}}" class="cambiarEstado btn btn-sm btn-primary">Cambiar
-                                        estado</a>
                                 </td>
                                 <td>
                                     <a class="btn btn-default btn-xs"
-                                       href="{{ route('proyectos.show', $c->id) }}">
+                                       href="{{ route('cliente.show', $p->id) }}">
                                         Ver
                                     </a>
 
@@ -50,5 +58,38 @@
             </div>
         </div>
     </div>
+
+    <script>
+        $(".cambiarEstado").click(function () {
+
+            var id = $(this).data('id');
+
+            $.ajax({
+                url: "http://vsrad.dev/proyectos/cambiarEstado/" + id,
+                context: document.body,
+                method: "get",
+                success: todoOk,
+                error: todoMal
+            });
+
+        });
+
+        function todoOk(data) {
+
+            var labelvalidado = $(".labelValidado-" + data.id);
+
+            labelvalidado.toggleClass("btn-primary btn-success");
+
+            labelvalidado.text("Pendiente de validación");
+
+            if (data.validado)
+                labelvalidado.text("Validado");
+        }
+
+        function todoMal() {
+            alert("Todo mal");
+        }
+
+    </script>
 
 @endsection
